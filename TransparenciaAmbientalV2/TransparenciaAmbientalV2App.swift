8ac89b40ae8b3eb0141a9240f14b4c5fad3057a0
Victor Uriarte <uriarte0505@gmail.com>
2025-08-30 18:46:23 -0300

    Initial Commit

---
+++ TransparenciaAmbientalV2/TransparenciaAmbientalV2App.swift
@@ -0,0 +1,17 @@
+//
+//  TransparenciaAmbientalV2App.swift
+//  TransparenciaAmbientalV2
+//
+//  Created by Victor Uriarte on 30/08/25.
+//
+
+import SwiftUI
+
+@main
+struct TransparenciaAmbientalV2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
